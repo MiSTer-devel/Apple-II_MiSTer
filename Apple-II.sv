3f0dc4e0e03046e4fcd20e7074fbd940d6719412
Sorgelig <pour.garbage@gmail.com>
2024-12-26 21:44:12 +0800

    Update sys.

--- Apple-II.sv
+++ Apple-II.sv
@@ -57,6 +57,7 @@ module emu
 	input  [11:0] HDMI_WIDTH,
 	input  [11:0] HDMI_HEIGHT,
 	output        HDMI_FREEZE,
+	output        HDMI_BLACKOUT,
 
 `ifdef MISTER_FB
 	// Use framebuffer in DDRAM
@@ -187,6 +188,7 @@ assign VGA_SCALER= 0;
 assign VGA_DISABLE = 0;
 assign VGA_F1    = 0;
 assign HDMI_FREEZE = 0;
+assign HDMI_BLACKOUT = 0;
 
 wire [1:0] ar = status[13:12];
 video_freak video_freak
